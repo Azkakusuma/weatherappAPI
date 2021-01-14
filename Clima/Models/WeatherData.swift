6f1911b3d1db5f0cc2c963578b0027b829e4e92c
azkakusuma <ahmad.azka.kusuma@mail.ugm.ac.id>
2021-01-14 14:58:09 +0700

    commit api

---
+++ Clima/Models/WeatherData.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Azka Kusuma Edy on 13/01/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/Models/weatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Azka Kusuma Edy on 12/01/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
